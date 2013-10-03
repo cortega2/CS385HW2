d2903e424e9d34c983d2dbb4939f1166e7ed9219
Carlos Ortega <corteg20@gmail.com>
2013-10-03 01:27:41 -0500

    Was able to read from exec. Still cannot send the buffer to exec.

--- master.c
+++ master.c
@@ -8,11 +8,20 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]){
-	int temp;
-	for(temp = 0; temp < argc; temp++){
-		printf("%s\n",argv[temp]);
+	if(argc<7){
+		printf("Not enought arguments\n");
+		return -1;
 	}
+	int randSeed = atoi(argv[5]);
+	int sleepMax = atoi(argv[4]);
+	int sleepMin = atoi(argv[3]);
+	int nWorkers = atoi(argv[2]);
 	
+	if(randSeed == 0)
+		srand(time(NULL));
+	else
+		srand(randSeed);	
+
 	//pipes + child
 	int pipe1[2];					//Parent reads/Child writes
 	int pipe2[2];					//Parent writes/Child reads
@@ -30,29 +39,58 @@ int main(int argc, char* argv[]){
 	child = fork();
 	//Parent
 	if(child > 0){
-		close(pipe1[0]);			//Not reading from pipe1
-		close(pipe2[1]);			//Not writing to pipe2
+		int savedSTDOUT = dup(1);
+		char buff[80];
+		char readBuff[80];
+
+		//char* testB = "1\n10\n3\n";
+
+		int t;
+		int b=0;
+		for(t =0; t < nWorkers; t++){
+			int b2 = sprintf(buff + b, "%d\n", (rand() % sleepMax + sleepMin));
+			b = b + b2;
+		}
+		printf("%s", buff);
+			
+		close(pipe1[0]);					//Not reading from pipe1
+		close(pipe2[1]);					//Not writing to pipe2
 		dup2(pipe2[0], 0);
 		dup2(pipe1[1], 1);
-		close(pipe2[0]);
+		
+		write(pipe1[1], buff, strlen(buff));
 		close(pipe1[1]);
+		
+		while(read(pipe2[0], readBuff, sizeof(readBuff)) != 0){
+		}
+
+		close(pipe2[0]);
+		wait(NULL);
+	
+		dup2(savedSTDOUT, 1);				//restore stdout
+		close(savedSTDOUT);
+		printf("\nReading:\n%s", readBuff);
 	}
 	//child
 	else if(child == 0){
-		close(pipe1[1]);			//Not writing to pipe1
-		close(pipe2[0]);			//Not reading from pipe2
+	
+		close(pipe1[1]);					//Not writing to pipe1
+		close(pipe2[0]);					//Not reading from pipe2
+		
 		dup2(pipe1[0], 0);
-		dup2(pipe2[1], 1);
 		close(pipe1[0]);
+				
+		dup2(pipe2[1], 1);
+		dup2(pipe2[1], 2);	
 		close(pipe2[1]);
-
-		char* command = {"sort", "-nr", NULL};
-		execvp(command[0], command);
+		
+		execlp("sort", "aort", "-nr", NULL);
+		printf("Something bad happened\n");
+		return;
 	}
 	//something bad happened
 	else{
 		printf("Fork failed!\n");
 		return -1;
-	}
-		
+	}		
 }
